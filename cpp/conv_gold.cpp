6daec1d435b86c9eff2d9023d232408442e1b7f9
kyle <khktseng@gmail.com>
2024-01-31 00:53:04 -0800

    Completed conv_gold and conv_gold_tiled

--- cpp/conv_gold.cpp
+++ cpp/conv_gold.cpp
@@ -9,15 +9,15 @@ void conv_gold(int16_t ifmap[(OY-1)*STRIDE+FY][(OX-1)*STRIDE+FX][IC],
                int32_t ofmap[OY][OX][OC]){
 
 	for (int oc = 0; oc < OC; oc++) {
-		for (int ic = 0; ic < IC; ic++) {
-			for (int oy = 0; oy < OY; oy++) {
-				for (int ox = 0; ox < OX; ox++) {
-					//ofmap[oy][ox][oc] = 0;
+		for (int oy = 0; oy < OY; oy++) {
+			for (int ox = 0; ox < OX; ox++) {
+				ofmap[oy][ox][oc] = 0;
+                for (int ic = 0; ic < IC; ic++) {
 					for (int fy = 0; fy < FY; fy++) {
 						for (int fx = 0; fx < FX; fx++) {
-							ofmap[oy][ox][oc] += (int32_t)
-								ifmap[STRIDE*oy + fy][STRIDE*ox + fx][ic] * (int32_t)
-								weight[fy][fx][ic][oc];
+							ofmap[oy][ox][oc] += 
+							(int32_t) ifmap[STRIDE*oy + fy][STRIDE*ox + fx][ic] * 
+							(int32_t) weight[fy][fx][ic][oc];
 						}}}}}}
 
 }
--- cpp/conv_gold_tiled.cpp
+++ cpp/conv_gold_tiled.cpp
@@ -8,5 +8,42 @@ void conv_gold_tiled(int16_t ifmap[(OY1*OY0-1)*STRIDE+FY][(OX1*OX0-1)*STRIDE+FX]
                int16_t weights[FY][FX][IC1*IC0][OC1*OC0],
                int32_t ofmap[OY1*OY0][OX1*OX0][OC1*OC0]) {
 
+	for (int i = 0; i < OY1*OY0; i++) {
+		for (int j = 0; j < OX1*OX0; j++) {
+			for (int k = 0; k < OC1*OC0; k++) {
+				ofmap[i][j][k] = 0;
+			}
+		}
+	}
 
+    // OY1, OX1, OC1, IC1, FY, FX, OY0, OX0, OC0, IC0
+    for (int oy1 = 0; oy1 < OY1; oy1++) {
+        for (int ox1 = 0; ox1 < OX1; ox1++) {
+            for (int oc1 = 0; oc1 < OC1; oc1++) {
+                for (int ic1 = 0; ic1 < IC1; ic1++) {
+                    for (int fy = 0; fy < FY; fy++) {
+                        for (int fx = 0; fx < FX; fx++) {
+                            for (int oy0 = 0; oy0 < OY0; oy0++) {
+                                for (int ox0 = 0; ox0 < OX0; ox0++) {
+                                    for (int oc0 = 0; oc0 < OC0; oc0++) {
+                                        int oy = oy1*OY0 + oy0;
+                                        int ox = ox1*OX0 + ox0;
+                                        int oc = oc1*OC0 + oc0;
+                                        for(int ic0 = 0; ic0 < IC0; ic0++) {
+                                            int ic = ic1*IC0 + ic0;
+
+											ofmap[oy][ox][oc] += 
+											(int32_t) ifmap[STRIDE*oy + fy][STRIDE*ox + fx][ic] * 
+											(int32_t) weights[fy][fx][ic][oc];
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
 }
